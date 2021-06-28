fb6da79708c348be3ac2f609459bdfd9e6500b9a
Jpeyton <jpwalk319@gmail.com>
2021-06-28 16:09:13 -0600

    added to server and models, added functions to ORM

---
+++ config/orm.js
@@ -0,0 +1,17 @@
+const connection = require('./connection');
+
+const ORM = {
+    selectAll(){
+
+    },
+
+    insertOne(){
+
+    },
+
+    updateOne(){
+
+    }
+};
+
+module.exports = ORM;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const exphbs = require("express-handlebars");
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+const db = require("./models");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+
+app.listen(PORT, () => {
+    console.log(`Listening on http://localhost:${PORT}/`);
+});
