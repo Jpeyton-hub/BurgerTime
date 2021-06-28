387c88c6188a8694d698f30408384639d4115b5b
Jpeyton <jpwalk319@gmail.com>
2021-06-28 16:58:20 -0600

    added to orm functions

--- config/orm.js
+++ config/orm.js
@@ -1,12 +1,21 @@
-const connection = require('./connection');
+const db = require('./connection');
+const mysql = require
 
 const ORM = {
     selectAll(){
-        
+        let sql = "SELECT * FROM shyt4u3hmkblfp79.burgers";
+        db.query(sql, (err, result) => {
+            if (err) throw err;
+            console.log("1 record inserted");
+          });
     },
 
-    insertOne(){
-
+    insertOne(burgerName){
+       let sql = "INSERT INTO burgers (burgerName) VALUES(?)";
+       db.query(sql, [burgerName], (err, result) => {
+        if (err) throw err;
+        console.log("1 record inserted");
+      });
     },
 
     updateOne(){
