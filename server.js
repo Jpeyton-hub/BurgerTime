6eb8f5fc15b25a7bd50e8cb3ac3b5838f9de67e6
Jpeyton <jpwalk319@gmail.com>
2021-07-06 11:47:03 -0600

    added some basic styling and layout

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const exphbs = require("express-handlebars");
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
-const db = require("./models");
+// const db = require("./models");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +13,10 @@ app.use(express.static("public"));
 
 app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
+
+app.get("/", (req, res) => {
+    res.render('index')
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on http://localhost:${PORT}/`);
